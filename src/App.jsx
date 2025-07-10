614f84002ab6149f01764abf5e452ac5d2bf3ae6
Sofiia <sonya17035@gmail.com>
2025-07-10 15:36:23 +0200

    add loader and fix btn color

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,27 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomeTab from "./pages/HomeTab/HomeTab";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
-import CatalogTab from "./pages/CatalogTab/CatalogTab";
-import DetailsTab from "./pages/DetailsTab/DetailsTab";
+// import CatalogTab from "./pages/CatalogTab/CatalogTab";
+// import DetailsTab from "./pages/DetailsTab/DetailsTab";
 import Header from "./components/Header/Header";
+import { lazy, Suspense } from "react";
+import Loader from "./components/Loader/Loader";
+
+const CatalogTab = lazy(() => import("./pages/CatalogTab/CatalogTab"));
+const DetailsTab = lazy(() => import("./pages/DetailsTab/DetailsTab"));
 
 function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<HomeTab />} />
-        <Route path="catalog" element={<CatalogTab />} />
-        <Route path="catalog/:id" element={<DetailsTab />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<HomeTab />} />
+          <Route path="catalog" element={<CatalogTab />} />
+          <Route path="catalog/:id" element={<DetailsTab />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
